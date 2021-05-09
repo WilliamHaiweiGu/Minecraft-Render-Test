3339d7a3368ef1063e01a2d63870498da1439baf
William Gu <williamhaiweigu@gmail.com>
2021-05-09 10:26:17 +0800

    Loaded data

---
+++ overviewerConfig.js
@@ -0,0 +1,338 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Render Test - overworld",
+        "Render Test - nether",
+        "Render Test - end"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1620478960",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "overworld NE",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld NE",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - overworld",
+            "last_rendertime": 1620470923,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": [
+                42,
+                80,
+                -17
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "overworld NW",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld NW",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - overworld",
+            "last_rendertime": 1620470923,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": [
+                42,
+                80,
+                -17
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "overworld SW",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld SW",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - overworld",
+            "last_rendertime": 1620470923,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": [
+                42,
+                80,
+                -17
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "overworld SE",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "overworld SE",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - overworld",
+            "last_rendertime": 1620470923,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": [
+                42,
+                80,
+                -17
+            ],
+            "north_direction": 3
+        },
+        {
+            "name": "nether NE",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "nether NE",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - nether",
+            "last_rendertime": 1620470900,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "nether NW",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "nether NW",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - nether",
+            "last_rendertime": 1620470900,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 1
+        },
+        {
+            "name": "nether SW",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "nether SW",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - nether",
+            "last_rendertime": 1620470900,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "nether SE",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "nether SE",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - nether",
+            "last_rendertime": 1620470900,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 3
+        },
+        {
+            "name": "end NE",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "end NE",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - end",
+            "last_rendertime": 1620470932,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "end NW",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "end NW",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - end",
+            "last_rendertime": 1620470932,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 1
+        },
+        {
+            "name": "end SW",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "end SW",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - end",
+            "last_rendertime": 1620470932,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 2
+        },
+        {
+            "name": "end SE",
+            "zoomLevels": 7,
+            "defaultZoom": 1,
+            "maxZoom": 7,
+            "path": "end SE",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Render Test - end",
+            "last_rendertime": 1620470932,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                42,
+                80,
+                -17
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 3
+        }
+    ]
+};
